c55b51e8ed79f907fbb9cccb05d635a12ccfc6ee
FlyingLawnmower <43915072+puggen1@users.noreply.github.com>
2023-01-03 12:34:15 +0100

    added jshint config file, added try catch, commented it out

--- apiCalls/createImage.js
+++ apiCalls/createImage.js
@@ -4,15 +4,29 @@ let apitoken = process.env.API_TOKEN;
 
 async function createImage(imgString, numberOfImages = 2, size = "1024x1024") {
   let numberImage = parseInt(numberOfImages);
+  let response;
   const configuration = new Configuration({
     apiKey: apitoken,
   });
   const openai = new OpenAIApi(configuration);
-  const response = await openai.createImage({
+  /*try{*/
+    response = await openai.createImage({
     prompt: imgString,
     n: numberImage,
     size: size,
   });
+/*}*/
+/*
+catch(error){
+  console.log(error);
+  if(error.response){
+    console.log(error.response.data);
+  }
+  else{
+    console.log(error.message);
+    response = error.message;
+  }
+}*/
   return response.data;
 }
 
